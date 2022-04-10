72d4c4ee26bbc4f072c2bc18f441f4e63d649843
Marissa Fandel <mfandel@alumni.nd.edu>
2022-04-10 16:13:25 -0600

    added PUT logic to database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,17 @@ const initdb = async () =>
 	});
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDB = async (content) => {};
+export const putDB = async (content) => {
+	console.log("PUT to database");
+
+	const jateDB = await openDB("jate", 1);
+	const tx = jateDB.transaction("jate", "readwrite");
+	const store = tx.objectStore("jate");
+	const req = store.put({ value: content });
+	const res = await req;
+
+	console.log("🚀 - data saved to the database", res);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDB = async () => {
